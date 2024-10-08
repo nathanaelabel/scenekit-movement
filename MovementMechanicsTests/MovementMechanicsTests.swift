ff68fe66a495b78a477efef4765f7e0cbe4f4b95
Nathan <nathancodelab@gmail.com>
2024-10-08 09:34:52 +0700

    Initial Commit

---
+++ MovementMechanicsTests/MovementMechanicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovementMechanicsTests.swift
+//  MovementMechanicsTests
+//
+//  Created by Nathanael Abel on 08/10/24.
+//
+
+import Testing
+@testable import MovementMechanics
+
+struct MovementMechanicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
